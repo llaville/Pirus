e89a0a79d2597a419ff46c1149decd68703928e7
Laurent Laville <laurent.laville@gmail.com>
2013-01-09 16:00:03 +0100

    new API 2.0, pirum forked code almost equals

--- scripts/pirus.bat
+++ scripts/pirus.bat
@@ -1,3 +1,19 @@
 @echo off
+REM Pirus
+REM (c) 2013 Laurent Laville
+REM Release: @pirus_version@
+REM GIT: $Id$
+REM
+REM Pirum
+REM (c) 2009-2012 Fabien Potencier
+REM
+REM For the full copyright and license information, please view the LICENSE
+REM file that was distributed with this source code.
 
-"@php_bin@" "@bin_dir@\pirus" %*
+if "%PHPBIN%" == "" set PHPBIN=@php_bin@
+if not exist "%PHPBIN%" if "%PHP_PEAR_PHP_BIN%" neq "" goto USE_PEAR_PATH
+GOTO RUN
+:USE_PEAR_PATH
+set PHPBIN=%PHP_PEAR_PHP_BIN%
+:RUN
+"%PHPBIN%" "pirus" %*
